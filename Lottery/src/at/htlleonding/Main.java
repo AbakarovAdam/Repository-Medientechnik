57aed4cc635311df7f2d67e3ef118a726edba4c0
Adam Abakarov <ramasanabakaro9@gmail.com>
2021-10-19 14:59:48 +0200

    Übung10/Übung

---
+++ Lottery/src/at/htlleonding/Main.java
@@ -0,0 +1,19 @@
+/* Author: Abakarov Adam
+   Date: 11.10.2021
+   Class: 2BHITM
+   Program: Lottery
+ */
+
+package at.htlleonding;
+import java.util.Scanner;
+public class Main {
+
+    public static int[] InputArray(int userInput){
+        int[] InputArray = new int[6];
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
